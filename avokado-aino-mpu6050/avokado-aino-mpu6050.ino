327b85c30034fc5fcc7cd1e3a02d6f12c26939e7
Alex Shure <shure@etemu.com>
2014-05-04 22:02:15 +0200

    rebrand to avokado at Franz-Künstler-Str

---
+++ avokado-aino-mpu6050/avokado-aino-mpu6050.ino
@@ -0,0 +1,547 @@
+#if 1 // BOF preprocessor bug workaround
+#define HW 1 // define hardware revision, 1=switchcube, 2=C3POWproto2, ... for different pin assignments and peripheral hardware
+#include "Arduino.h"
+inline unsigned long get_last_time();
+inline float get_last_x_angle();
+inline float get_last_y_angle();
+inline float get_last_z_angle();
+inline float get_last_gyro_x_angle();
+inline float get_last_gyro_y_angle();
+inline float get_last_gyro_z_angle();
+void setup();
+void active();
+void loop();
+void wilssen();
+unsigned long microsRollover();
+void ledst(int sta);
+void colorWipe(uint32_t c, uint8_t wait);
+void set_last_read_angle_data(unsigned long time, float x, float y, float z, float x_gyro, float y_gyro, float z_gyro);
+void calibrate_sensors();
+void getangle();
+void send_K(unsigned int to);
+boolean send_L(uint16_t to, byte* ledmap);
+void processPacket();
+#line 2
+__asm volatile ("nop"); // BOF preprocessor bug workaround
+#endif
+
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of the project
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include <avr/sleep.h>
+#include <avr/power.h>
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
+#include <stdarg.h>
+#include <EEPROM.h>
+#include <CapacitiveSensor.h>
+#define DEBUG 1 // debug mode with verbose output over serial at 115200 bps
+#define USE_EEPROM // read nodeID and network settings from EEPROM at bootup, overwrites nodeID and MAC.
+#define LEDPIN 6
+#define KEEPALIVE 0 // keep connections alive with regular polling to node 0
+//#define USE_LEDS // LED stripe used
+//#define USE_TOUCH // capacitive touch sensing
+#define TIMEOUT_HIBERNATE 5000
+#define TIMEOUT_MEMS 5000
+#define TIMEOUT_RADIO 8000
+#define PIN_VIBR 5 // pin for vibration motor, 9 for switchcube1, 5 for switch
+
+#ifdef USE_LEDS
+#include <Adafruit_NeoPixel.h>
+Adafruit_NeoPixel leds = Adafruit_NeoPixel(8, LEDPIN, NEO_GRB + NEO_KHZ800); // number of pixels in strip, pin number, pixel type flags
+#endif
+RF24 radio(A0, 10); // CE, CS. CE at pin A0, CSN at pin 10
+RF24Network network(radio); // mesh network layer
+
+#ifdef USE_TOUCH
+CapacitiveSensor cs = CapacitiveSensor(8, 7);       // capacitive sensing at pins 7 and 8
+#endif
+
+const unsigned long interval_filter = 32;
+const unsigned long interval = 50; // KEEPALIVE interval in [ms]
+unsigned long last_time_filtered;
+byte sweep = 0;
+byte nodeID = 1; // Unique Node Identifier (2...254) - also the last byte of the IPv4 adress, not used if USE_EEPROM is set
+uint16_t this_node = 00; // always begin with 0 for octal declaration, not used if USE_EEPROM is set
+// Debug variables, TODO: don't initialize if DEBUG is set to 0
+unsigned long iterations = 0;
+unsigned long errors = 0;
+unsigned int loss = 0;
+unsigned long p_sent = 0;
+unsigned long p_recv = 0;
+// Variables for the 32bit unsigned long Microsecond rollover handling
+static unsigned long microRollovers = 0; // variable that permanently holds the number of rollovers since startup
+static unsigned long halfwayMicros = 2147483647; // this is halfway to the max unsigned long value of 4294967296
+static boolean readyToRoll = false; // tracks whether we've made it halfway to rollover
+
+const short max_active_nodes = 32; // the size of the array with active nodes
+uint16_t active_nodes[max_active_nodes];
+short num_active_nodes = 0;
+short next_ping_node_index = 0;
+unsigned long last_time_sent;
+unsigned long updates = 0; // has to be changed to unsigned long if the interval is too long
+void add_node(uint16_t node);
+boolean send_T(uint16_t to);
+void send_L1(int to, int _b);
+void handle_K(RF24NetworkHeader& header);
+void handle_L(RF24NetworkHeader& header);
+void handle_T(RF24NetworkHeader& header);
+void handle_B(RF24NetworkHeader& header);
+void ledupdate(byte* ledmap);
+void p(char *fmt, ... );
+void ledst(int sta = 127);
+void mpucheck();
+bool strobe = 1;
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default)
+// AD0 high = 0x69
+MPU6050 mpu;
+//MPU6050 mpu(0x69); // alternative I2C address, if AD0 is set high
+
+int16_t ax, ay, az; // accel values
+int16_t gx, gy, gz; // gyro values
+
+#define OUTPUT_READABLE_mpu // tab-separated list of the accel X/Y/Z and then gyro X/Y/Z values in decimal.
+// #define OUTPUT_BINARY_mpu //send all 6 axes of data as 16-bit
+
+unsigned long last_read_time;
+float         last_x_angle;  // These are the filtered angles
+float         last_y_angle;
+float         last_z_angle;
+float         last_gyro_x_angle;  // Store the gyro angles to compare drift
+float         last_gyro_y_angle;
+float         last_gyro_z_angle;
+
+inline unsigned long get_last_time() {
+  return last_read_time;
+}
+inline float get_last_x_angle() {
+  return last_x_angle;
+}
+inline float get_last_y_angle() {
+  return last_y_angle;
+}
+inline float get_last_z_angle() {
+  return last_z_angle;
+}
+inline float get_last_gyro_x_angle() {
+  return last_gyro_x_angle;
+}
+inline float get_last_gyro_y_angle() {
+  return last_gyro_y_angle;
+}
+inline float get_last_gyro_z_angle() {
+  return last_gyro_z_angle;
+}
+
+//  Use the following global variables and access functions
+//  to calibrate the acceleration sensor
+float    base_x_accel;
+float    base_y_accel;
+float    base_z_accel;
+
+float    base_x_gyro;
+float    base_y_gyro;
+float    base_z_gyro;
+
+// Sleep declarations
+typedef enum { wdt_16ms = 0, wdt_32ms, wdt_64ms, wdt_128ms, wdt_250ms, wdt_500ms, wdt_1s, wdt_2s, wdt_4s, wdt_8s } wdt_prescalar_e;
+void setup_watchdog(uint8_t prescalar);
+void do_sleep(void);
+const short sleep_cycles_per_transmission = 10;
+volatile short sleep_cycles_remaining = sleep_cycles_per_transmission;
+void powerMode(byte _mode);
+
+void vibr(byte _state);
+void pulse(unsigned int _length=50);
+bool checkTouch(unsigned int _sensitivity = 96);
+byte mode = 0;
+unsigned long lastActive = 0;
+
+void setup() {
+  // ONLY for C3POW prototype 2:
+  #if HW == 2
+    pinMode(7,OUTPUT);
+    digitalWrite(7, HIGH); // Vcc for MPU6050
+    pinMode(8, OUTPUT); // Vcc for the NRF24 module
+    digitalWrite(8, HIGH); // Vcc for the NRF24 module activated. Shutdown and hard power cut-off with LOW. Need to be initialized again afterwards.
+  #endif
+  pinMode(PIN_VIBR, OUTPUT); // vibration motor
+  vibr(0); // pull low and disable motor for now
+  vibr(1); // vibrate during bootup
+#ifdef USE_LEDS
+  leds.begin(); // the 8 LEDs
+  leds.show(); // Initialize all pixels to 'off'
+  ledst(5); // set initial status to 5, ledst() sets the first
+#endif
+#ifdef USE_TOUCH
+  cs.set_CS_AutocaL_Millis(10000);
+#endif
+  Serial.begin(115200); // initialize serial communication
+  delay(64);
+  vibr(0); // stop vibrating
+  // initialize devices
+  Serial.println(F("C3POW 0.201402190308"));
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Serial.println(F("I2CSetup"));
+  Wire.begin(); // join I2C bus (I2Cdev library doesn't do this automatically)
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Serial.println(F("FastwireSetup"));
+  Fastwire::setup(400, true);
+#endif
+  Serial.println(F("mpu.initialize"));
+  mpu.initialize();
+  ledst(4);
+#ifdef USE_EEPROM
+  nodeID = EEPROM.read(0);
+  Serial.print(F("EEPROM, "));
+#endif
+  Serial.println(nodeID);
+  Serial.print(F("Network ID (oct): "));
+#ifdef USE_EEPROM
+  this_node = ((int) EEPROM.read(15) * 256) + ((int) (EEPROM.read(16)));
+  Serial.print(F("EEPROM, "));
+#endif
+  Serial.print(this_node, OCT);
+  Serial.print(F(", (dec): "));
+  Serial.print(this_node, DEC);
+  Serial.println();
+  SPI.begin(); // SPI for the NRF24
+  radio.begin(); // init of the NRF24
+  // The amplifier gain can be set to RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_MED=-6dBM, and RF24_PA_MAX=0dBm.
+  radio.setPALevel(RF24_PA_MAX); // transmitter gain value (see above)
+  Serial.print(F("PA-level: "));
+  Serial.print(radio.getPALevel());
+  Serial.print(F("\t CRC: "));
+  Serial.print(radio.getCRCLength());
+  Serial.print(F("\t datarate: "));
+  Serial.println(radio.getDataRate());
+  network.begin( 1, this_node ); // fixed radio channel, node ID
+  Serial.print(F("UID: "));
+  Serial.print(F("(undefined)\n"));
+  p("%010ld: Starting up\n", millis());
+#ifdef USE_LEDS
+  colorWipe(leds.Color(50, 0, 0), 50); // Red
+  colorWipe(leds.Color(0, 50, 0), 50); // Green
+  colorWipe(leds.Color(0, 0, 50), 50); // Blue
+  colorWipe(leds.Color(0, 0, 0), 0); // clear
+  ledst(5);
+#endif
+
+  // verify connection
+  Serial.println(F("Init I2C..."));
+  Serial.println(mpu.testConnection() ? "MPU6050 connected." : "MPU6050 error.");
+  calibrate_sensors();
+  set_last_read_angle_data(millis(), 0, 0, 0, 0, 0, 0);
+#ifdef USE_LEDS
+  ledst(1);
+#endif
+  setup_watchdog(wdt_1s); // Set the watchdog timer interval
+  radio.powerUp();
+}
+
+void powerMode(byte _mode = mode) {
+  switch (_mode) // check which power mode should be set active
+  {
+    case 0:
+      break; // no change in power mode
+    case 1:
+      mpu.setSleepEnabled(0); // activate the MEMS device
+      Serial.println(F("powerMode(1), MEMS up"));
+      mode = 0;
+      break;
+    case 2:
+      radio.powerUp(); // activate the radio
+      Serial.println(F("powerMode(2), radio up"));
+      mode = 0;
+      break;
+    case 5:
+      Serial.println(F("powerMode(5), hibernate"));
+      radio.powerDown();
+      mpu.setSleepEnabled(1);
+      mpu.setStandbyXGyroEnabled(1);
+      mpu.setStandbyYGyroEnabled(1);
+      mpu.setStandbyZGyroEnabled(1);
+      vibr(1);
+      delay(64);
+      vibr(0);
+      delay(196);
+      vibr(1);
+      delay(64);
+      vibr(0);
+      setup_watchdog(wdt_1s); // set touch check interval
+      while ( ay < (-10000) ) { // if the C3POW device is resting in this position
+        do_sleep(); // keep on sleeping
+        powerMode(1); // power up MEMS, wake up every watchdog timer interval
+        //        mpucheck();
+        mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz); // get the new acceleration values TODO: implement for ADXL345
+      }
+      for (int _i = 0; _i < 2; _i++) { // vibrate briefly when waking up from hibernation mode
+        vibr(1);
+        delay(128);
+        vibr(0);
+        delay(56);
+      }
+      powerMode(1); // power up MEMS
+      mpu.setStandbyXGyroEnabled(0);
+      mpu.setStandbyYGyroEnabled(0);
+      mpu.setStandbyZGyroEnabled(0);
+      powerMode(2); // power up radio
+      active();
+      break;
+    case 6: // shutdown until next hard reset
+      Serial.println(F("powerMode(6), shutdown"));
+      radio.powerDown();
+      mpu.setSleepEnabled(1);
+      vibr(4); // descending heartbeat pattern
+      delay(512);      
+      vibr(4);
+      delay(700);      
+      vibr(4);
+      delay(900);      
+      vibr(4);
+      delay(1100);      
+      vibr(5);
+      delay(1280);      
+      vibr(5);
+      delay(1650);      
+      pulse(32);
+      setup_watchdog(wdt_8s); // longest watchdog interval
+      while(1) do_sleep(); // sleep forever
+      break;      
+    default: // no change in power mode
+      return;
+  };
+}
+
+bool checkTouch(unsigned int _sensitivity)
+{
+#ifdef USE_TOUCH
+  long csVal = cs.capacitiveSensor(24);
+  if (csVal > _sensitivity) {
+    //send_L1(01,_val);
+    //Serial.println(csVal);
+    return 1;
+  }
+#endif
+  return 0;
+}
+
+void active() {
+  lastActive = millis();
+}
+
+void loop() {
+  
+  /////////////////////////
+  //powerMode(6); // WIP - shutdown forever, disable hardware until next hard reset
+  /////////////////////////
+  
+  if (millis() - lastActive > TIMEOUT_HIBERNATE && (ay < (-10000))) powerMode(5);
+  //  if (millis()-lastActive> TIMEOUT_MEMS) mpu.setSleepEnabled(1);
+  //  if (millis()-lastActive> TIMEOUT_RADIO) radio.powerDown();
+  wilssen();
+  network.update();
+  updates++;
+  while ( network.available() ) // while there are packets in the FIFO buffer
+  {
+    processPacket();
+  }
+
+  unsigned long now = millis();
+  unsigned long nowM = micros();
+  if ( now - last_time_filtered >= interval_filter ) { // non-blocking check for start of debug service routine interval
+    getangle();
+    last_time_filtered = now;
+  }
+
+  if ( now - last_time_sent >= interval ) // non-blocking check for start of debug service routine interval
+  {
+    ledst(2);
+    /* // unsigned long int rollover checking:
+     Serial.print(microsRollover()); // how many times has the unsigned long micros() wrapped?
+     Serial.print(":"); //separator
+     Serial.print(nowM); //micros();
+     Serial.print("\n"); //new line
+     */
+    if (DEBUG) {
+      p("%010ld: %ld Hz\n", millis(), updates * 1000 / interval);
+    }
+    updates = 0;
+    last_time_sent = now;
+    if (KEEPALIVE) {
+      uint16_t to = 000;
+      bool ok = 0;
+      if ( to != this_node)
+      {
+        unsigned long nowM = micros();
+        ok = send_T(to);
+        if (DEBUG) p(" in %ld us.\n", (micros() - nowM) );
+        if (ok) p_sent++;
+        if (!ok)
+        {
+          errors++;
+          if (DEBUG) {
+            p("%010ld: ACK timeout.\n", millis()); // An error occured, need to stahp!
+          }
+        }
+        iterations++;
+      }
+    }
+    sweep += 1;
+    if (sweep > 254) sweep = 0;
+    strobe = !strobe;
+    if ( this_node == 00) {
+      for (short _i = 0; _i < num_active_nodes; _i++) {
+        send_K(active_nodes[_i]);
+      }
+    }
+  }
+
+  /*
+
+  */
+  /*
+  radio.powerDown();
+  Serial.print(F("MotionDetectionDuration: "));
+  Serial.print(mpu.getMotionDetectionDuration());
+  Serial.print(F("\t Threshold: "));
+  Serial.print(mpu.getMotionDetectionThreshold());
+  Serial.print(F("\t gyroRange: "));
+  Serial.print(mpu.getFullScaleGyroRange());
+  Serial.print(F("\t getClockSource: "));
+  Serial.print(mpu.getClockSource());
+  Serial.print(F("\n"));
+  Serial.print(F("Radio down.\n"));
+  delay(1000);
+
+  Serial.print(F("+MCU into sleep mode for 10s.\n"));
+  delay(50);
+  while ( sleep_cycles_remaining ) do_sleep(); // Sleep the MCU, the watchdog timer will awaken in a short while.
+  sleep_cycles_remaining = sleep_cycles_per_transmission;
+
+  Serial.print(F("+Xgyro now sleeping for 10s.\n"));
+  mpu.setStandbyXGyroEnabled(1);
+  delay(50);
+  while ( sleep_cycles_remaining ) do_sleep(); // Sleep the MCU, the watchdog timer will awaken in a short while.
+  sleep_cycles_remaining = sleep_cycles_per_transmission;
+
+  Serial.print(F("+Ygyro now sleeping for 10s.\n"));
+  mpu.setStandbyYGyroEnabled(1);
+  delay(50);
+  while ( sleep_cycles_remaining ) do_sleep(); // Sleep the MCU, the watchdog timer will awaken in a short while.
+  sleep_cycles_remaining = sleep_cycles_per_transmission;
+
+  Serial.print(F("+Zgyro now sleeping for 10s.\n"));
+  mpu.setStandbyZGyroEnabled(1);
+  delay(50);
+  while ( sleep_cycles_remaining ) do_sleep(); // Sleep the MCU, the watchdog timer will awaken in a short while.
+  sleep_cycles_remaining = sleep_cycles_per_transmission;
+
+  Serial.print(F("+setSleepEnabled(1) for 10s.\n"));
+  mpu.setSleepEnabled(1);
+  delay(50);
+  while ( sleep_cycles_remaining ) do_sleep(); // Sleep the MCU, the watchdog timer will awaken in a short while.
+  sleep_cycles_remaining = sleep_cycles_per_transmission;
+
+  Serial.print(F("MCU now sleeping for 10s with power to radio cut off.\n"));
+  delay(127);
+  while ( sleep_cycles_remaining ) do_sleep(); // Sleep the MCU, the watchdog timer will awaken in a short while.
+  Serial.print(F("MCU awake. 1s delay to radio powerup.\n"));
+  digitalWrite(8, HIGH);
+  digitalWrite(7, HIGH);
+  delay(1000);
+  mpu.setSleepEnabled(0);
+  radio.powerUp();
+  Serial.print(F("Radio up.\n"));
+  sleep_cycles_remaining = sleep_cycles_per_transmission;
+  */
+}
+
+// 0=16ms, 1=32ms,2=64ms,3=125ms,4=250ms,5=500ms
+// 6=1 sec,7=2 sec, 8=4 sec, 9= 8sec
+void setup_watchdog(uint8_t prescalar)
+{
+  prescalar = min(9, prescalar);
+  uint8_t wdtcsr = prescalar & 7;
+  if ( prescalar & 8 )
+    wdtcsr |= _BV(WDP3);
+  MCUSR &= ~_BV(WDRF);
+  WDTCSR = _BV(WDCE) | _BV(WDE);
+  WDTCSR = _BV(WDCE) | wdtcsr | _BV(WDIE);
+}
+
+ISR(WDT_vect)
+{
+  --sleep_cycles_remaining;
+}
+
+void do_sleep(void)
+{
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
+  sleep_enable();
+  sleep_mode();                        // System sleeps here
+  sleep_disable();                     // System continues execution here when watchdog timed out
+}
+
+
+void wilssen() {
+
+}
+
+// Arduino version of the printf()-funcition in C
+void p(char *fmt, ... ) {
+  char tmp[128]; // resulting string limited to 128 chars
+  va_list args;
+  va_start (args, fmt );
+  vsnprintf(tmp, 128, fmt, args);
+  va_end (args);
+  Serial.print(tmp);
+}
+
+void add_node(uint16_t node) //TODO: remove_node, after a certain timeout...
+{
+  short i = num_active_nodes;
+  while (i--)
+    if ( active_nodes[i] == node ) break; // Do we already know about this node?
+  if ( i == -1 && num_active_nodes < max_active_nodes )  // If not and there is enough place, add it to the table
+  {
+    active_nodes[num_active_nodes++] = node;
+    if (DEBUG) {
+      p("%010ld: New node: %05o\n", millis(), node);
+    }
+  }
+}
+
+unsigned long microsRollover() { //based on Rob Faludi's (rob.faludi.com) milli wrapper
+
+  // This would work even if the function were only run once every 35 minutes, though typically,
+  // the function should be called as frequently as possible to capture the actual moment of rollover.
+  // The rollover counter is good for over 584000 years of runtime.
+  //  --Alex Shure
+
+  unsigned long nowMicros = micros(); // the time right now
+
+  if (nowMicros > halfwayMicros) { // as long as the value is greater than halfway to the max
+    readyToRoll = true; // we are in the second half of the current period and ready to roll over
+  }
+
+  if (readyToRoll == true && nowMicros < halfwayMicros) {
+    // if we've previously made it to halfway
+    // and the current millis() value is now _less_ than the halfway mark
+    // then we have rolled over
+    microRollovers++; // add one to the count of rollovers (approx 71 minutes)
+    readyToRoll = false; // we're no longer past halfway, reset!
+  }
+  return microRollovers;
+}
---
+++ avokado-aino-mpu6050/packet_types.ino
@@ -0,0 +1,305 @@
+/* WIP DRAFT, TBD
+ C voltage (1-24 bytes) fixed point values
+ D current (1-24 bytes) fixed point values
+ E battery voltage (2 bytes)
+ F error code (1 byte) + error value (1 byte)
+ G 
+ H reserved for home automation packet
+ L LED map
+ T send out timestamp
+ V software version, UID, wID, location
+ B reply with the just received timestamp (T->B)
+ */
+
+byte br=50; // global brightness setting
+byte _r,_g,_b,_c1,_c2,_c3=50;
+const int PROGMEM breakpoint = 32;
+void send_K(unsigned int to){
+  byte _actions=0;
+  unsigned int GMAX = 32000;
+  mpucheck();
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+//  unsigned int _br= (unsigned int) angle_z * 728 / 256 // brightness determined by the z_rotation
+
+if ( ay>(+15000) ) {
+  Serial.println(F("RIGHT"));
+  int _cd= abs(gy)-breakpoint; // threshhold for movement detection and against gyro drift
+  _cd=(max(_cd,breakpoint))-breakpoint;
+  _cd=map(_cd,0,GMAX,0,255);
+  if (gy>0) {
+    if (((int)_c1-_cd)<0) {
+    _c1=0;
+    vibr(2);
+    _actions++;
+  } else {_c1-=_cd;}
+  }
+  if (gy<=0) {
+    if (((int)_c1+_cd)>255) {_c1=255; vibr(2);_actions++;} else {_c1+=_cd;}
+  }
+ }
+
+if ( ay<(-10000) ) { // switchcube prototype 3 = top
+  Serial.println(F("LEFT/TOP"));
+
+//   long _csVal = cs.capacitiveSensor(32);
+//   Serial.println(_csVal);
+//   _csVal=min(_csVal,2048);
+//   br=map(_csVal,10,2048,5,255);
+//   if (_csVal > 512) _actions++;
+ }
+
+if ( ax>(+15000) ) {
+  Serial.println(F("BACK"));
+  int _cd= abs(gx)-breakpoint; 
+  _cd=(max(_cd,breakpoint))-breakpoint;
+  _cd=map(_cd,0,GMAX,0,255);
+  if (gx>0) {
+    if (((int)_c3-_cd)<0) {_c3=0;vibr(2);_actions++;} else {_c3-=_cd;}
+  }
+  if (gx<=0) {
+    if (((int)_c3+_cd)>255) {_c3=255;vibr(2);_actions++;} else {_c3+=_cd;}
+  }
+ }
+
+if ( ax<(-15000) ) {
+  Serial.println(F("FRONT"));
+  int _cd= abs(gx)-breakpoint; 
+  _cd=(max(_cd,breakpoint))-breakpoint;
+  _cd=map(_cd,0,GMAX,0,255);
+  if (gx>0) {
+    if (((int)_c2+_cd)>255) {_c2=255;vibr(2);_actions++;} else {_c2+=_cd;}
+  }
+  if (gx<=0) {
+    if (((int)_c2-_cd)<0) {_c2=0;vibr(2);_actions++;} else {_c2-=_cd;}
+  }
+ }
+
+if ( az>15000 ) {
+  Serial.println(F("TOP"));
+  int _cd= abs(gz)-breakpoint; 
+  _cd=(max(_cd,breakpoint))-breakpoint;
+  _cd=map(_cd,0,GMAX,0,255);
+  if (gz>0) {
+    if (((int)br-_cd)<0) {br=0;vibr(2);_actions++;} else {br-=_cd;}
+  }
+  if (gz<=0) {
+    if (((int)br+_cd)>255) {br=255;vibr(2);_actions++;} else {br+=_cd;}
+  }
+ }
+ 
+ if ( az< (-15000) ) {
+  Serial.println(F("BOT"));
+  
+  if (strobe) br=255;
+  if (!strobe) br=0;
+  _actions++;
+ }
+ Serial.println(F("ACT"));
+ Serial.println(_actions,DEC);
+ Serial.println(F("ACT"));
+ if (_actions>0) active();
+ 
+if ( (abs(angle_x)<10) & (abs(angle_y)<10) ) {
+  }
+  
+//byte _br=max(0,angle_z);
+  Serial.print(br);
+//  byte _c1= (unsigned int) (angle_x+90) * 364 / 257; // color
+//  byte _c2= (unsigned int) (angle_y+90) * 364 / 257; // color
+  _r= (unsigned int) _c1*br/255;
+  _g= (unsigned int) _c2*br/255;
+  _b= (unsigned int) _c3*br/255;
+  Serial.print(F("\t _r: "));
+  Serial.print(_r);
+  Serial.print(F("\t _g: "));  
+  Serial.print(_g);
+  Serial.print(F("\t _b: "));    
+  Serial.println(_b);
+/*  byte axl=map(abs(ax),0,17000,0,255);
+  byte ayl=map(abs(ay),0,17000,0,255);
+  byte azl=map(abs(az),0,17000,0,255);
+  byte gxl=map(2*abs(gx),0,32767,0,255);
+  byte gyl=map(2*abs(gy),0,32767,0,255);
+  byte gzl=map(2*abs(gz),0,32767,0,255);
+  */
+/*  byte kmap[24]={
+    000,000,000, // status LED at 0
+    axl&0xFF,ayl&0xFF,azl&0xFF, // acc values
+    axl&0xFF,ayl&0xFF,azl&0xFF,
+    axl&0xFF,ayl&0xFF,azl&0xFF,
+    axl&0xFF,ayl&0xFF,azl&0xFF,    
+    gxl,gxl,gxl,
+    gyl,gyl,gyl,
+    gzl,gzl,gzl };
+*/
+byte kmap[24]={
+    _r,_g,_b, // status LED at 0
+    _r,_g,_b,
+    _r,_g,_b,
+    _r,_g,_b,
+    _r,_g,_b,
+    _r,_g,_b,
+    _r,_g,_b,
+    _r,_g,_b};
+    unsigned long now = millis();
+    bool ok = send_L(to, kmap);
+    if (DEBUG) {
+      p(" in %ld ms.\n", (millis()-now) );
+      if (ok){}
+      if (!ok) p("%010ld: send_K timout.\n", millis()); // An error occured..
+    }
+  }
+ 
+void send_L1(int to, int _b = 0){
+  if ( to != this_node) {      
+    byte ledmap[24]={
+      1,2,3,
+      0,0,_b,
+      0,0,_b,
+      0,0,_b,
+      0,0,_b,
+      0,0,_b,
+      0,0,_b,
+      0,(((byte) millis()&0xFF)/10),_b        };
+
+    unsigned long now = millis();
+    bool ok = send_L(to, ledmap);
+    if (DEBUG) {
+      p(" in %ld ms.\n", (millis()-now) );
+      if (ok){}
+      if (!ok) p("%010ld: send_L1 timout.\n", millis()); // An error occured..
+    }
+    ledst();
+  }
+}
+
+boolean send_T(uint16_t to) // Send out this nodes' time -> Timesync!
+{
+  if (DEBUG) p("%010ld: Sent 'T' to   %05o", millis(),to);
+  RF24NetworkHeader header(to,'T');
+  unsigned long time = micros();
+  return network.write(header,&time,sizeof(time));
+}
+
+boolean send_L(uint16_t to, byte* ledmap) // Send out an LED map
+{
+  if (DEBUG) p("%010ld: 'L' to   %05o", millis(),to);
+  RF24NetworkHeader header(to,'L');
+  return network.write(header,ledmap,24);
+}
+
+#ifdef USE_LEDS
+void ledupdate(byte* ledmap){
+  for(uint8_t i=0; i<leds.numPixels(); i++) {
+    uint32_t c = leds.Color(ledmap[i*3],ledmap[(i*3)+1],ledmap[(i*3)+2]);
+    leds.setPixelColor(i, c);
+  }
+  leds.show();
+}
+#endif
+
+void handle_K(RF24NetworkHeader& header)
+{
+  byte kmap[24];
+  network.read(header,kmap,sizeof(kmap));
+  if (DEBUG) p("%010ld: 'K' from %05o\n", millis(), header.from_node);
+  byte ledmap[24]={
+    000,000,000, // status LED at 0
+    kmap[0],kmap[1],kmap[2], // acc values
+    kmap[3],kmap[3],kmap[3], // gyro x
+    kmap[4],kmap[4],kmap[4], // gyro y...
+    kmap[5],kmap[5],kmap[5],    
+    000,000,00,
+    000,000,00,
+    000,000,0      };
+  #ifdef USE_LEDS
+  ledupdate(ledmap);
+  #endif
+
+  if (DEBUG) {
+    for(uint16_t i=0; i<sizeof(kmap); i++) { // print out the received packet via serial
+      Serial.print(kmap[i]);
+      Serial.print(" ");
+    }
+    Serial.println();
+  }
+}
+
+void handle_L(RF24NetworkHeader& header)
+{
+  byte ledmap[24];
+  network.read(header,ledmap,sizeof(ledmap));
+  if (DEBUG) {
+    p("%010ld: 'L' from %05o\n", millis(), header.from_node);
+  }
+  #ifdef USE_LEDS
+  ledupdate(ledmap);
+  #endif
+  if (DEBUG) {
+    for(uint16_t i=0; i<sizeof(ledmap); i++) { // print out the received packet via serial
+      Serial.print(ledmap[i]);
+      Serial.print(" ");
+    }
+    Serial.println();
+  }
+}
+
+void handle_T(RF24NetworkHeader& header)
+{
+  unsigned long time;
+  network.read(header,&time,sizeof(time));
+  if (DEBUG) {
+    p("%010ld: 'T' from %05o:%010ld\n", millis(), header.from_node, time);
+  }
+  add_node(header.from_node);  
+  if(header.from_node != this_node)
+  {
+    RF24NetworkHeader header2(header.from_node/*header.from_node*/,'B');
+    unsigned long nowM = micros();
+    if(network.write(header2,&time,sizeof(time)))
+      if (DEBUG) {
+        p("%010ld: ->'B' to   %05o in ", millis(),header.from_node);
+        Serial.print(micros()-nowM-16);
+        Serial.print(F(" us.\n"));
+      }
+  }
+}
+
+void handle_B(RF24NetworkHeader& header)
+{
+  p_recv++;
+  unsigned long ref_time;
+  network.read(header,&ref_time,sizeof(ref_time));
+  if (DEBUG) {
+    p("%010ld: Recv 'B' from %05o -> %ldus round trip\n", millis(), header.from_node, micros()-ref_time);
+  }
+}
+void processPacket(){
+    ledst(3); // light up status LED with pattern #3
+    RF24NetworkHeader header; // initialize header
+    network.peek(header); // preview the header, but don't advance nor flush the packet
+    switch (header.type) // check which packet type we received
+    {
+      case 'K':
+        handle_K(header);
+        break;
+      case 'L':
+        handle_L(header);
+        break;
+      case 'T':
+        handle_T(header);
+        break;
+      case 'B':
+        handle_B(header);
+        break;
+      default:
+        network.read(header, 0, 0); // if none of the above packet types matched, read out and flush the buffer
+        if (DEBUG) {
+          Serial.print(F("undefined packet type: ")); // print the unrecognized packet type
+          Serial.print(header.type);
+          Serial.println();
+        }
+        break;
+    };
+    ledst(); // reset the status LED to the default pattern
+}
